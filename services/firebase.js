7fce1316305f748e005f6a919e9d6ff763d184fe
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-03-22 11:25:54 +0530

    styles and services updated

---
+++ services/firebase.js
@@ -0,0 +1,97 @@
+import { firebase, FieldValue } from "../lib/firebase";
+
+export async function doesUsernameExist(username) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("username", "==", username)
+    .get();
+
+  return result.docs.map((user) => user.data().length > 0);
+}
+// get user form the firestore where userid = userId (passed from the auth)
+export async function getUserByUserId(userId) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("userId", "==", userId)
+    .get();
+
+  const user = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+  return user;
+}
+
+export async function getSuggestedProfiles(userId, following) {
+  const result = await firebase.firestore().collection("users").limit(10).get();
+
+  return result.docs
+    .map((user) => ({ ...user.data(), docId: user.id }))
+    .filter(
+      (profile) =>
+        profile.userId !== userId && !following.includes(profile.userId)
+    );
+}
+// updateLoggedInUserFollowing,
+//   updateFollowedUserFollowers,
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId, //currently logged in user doc id
+  profileId, //the user that someone requests to follow
+  isFollowingProfile //true/false (am i currently following this person?)
+) {
+  return firebase
+    .firestore()
+    .collection("users")
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowingProfile
+        ? FieldValue.arrayRemove(profileId)
+        : FieldValue.arrayUnion(profileId),
+    });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId, //currently logged in user doc id
+  loggedInUserDocId, //the user that someone requests to follow
+  isFollowingProfile //true/false (am i currently following this person?)
+) {
+  return firebase
+    .firestore()
+    .collection("users")
+    .doc(profileDocId)
+    .update({
+      followers: isFollowingProfile
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId),
+    });
+}
+
+export async function getPhotos(userId, following) {
+  const result = await firebase
+    .firestore()
+    .collection("photos")
+    .where("userId", "in", following)
+    .get();
+
+  const userFollowedPhotos = result.docs.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id,
+  }));
+
+  const photoWithUserDetails = await Promise.all(
+    userFollowedPhotos.map(async (photo) => {
+      let userLikedPhoto = false;
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true;
+      }
+      const user = await getUserByUserId(photo.userId);
+      const { username } = user[0];
+
+      return { username, ...photo, userLikedPhoto };
+    })
+  );
+  return photoWithUserDetails;
+}
